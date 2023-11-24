38b96e5251fdd895c0453f98a86cbb535a0ac166
Songül Çelik <songulcelik3537@gmail.com>
2023-11-24 22:05:31 +0300

    d1

---
+++ day10/js/44-script.js
@@ -0,0 +1,33 @@
+//declaration func
+function topla(a,b){
+    return a+b;
+}
+
+console.log(topla(3,6));
+
+//expression func
+
+let carpma= function(a,b){
+    return a*b;
+}
+
+console.log(carpma(4,6));
+
+//arrow function
+let yasHesapla=(dogumYili)=> 2023-dogumYili;
+
+console.log(yasHesapla(2000));
+
+const sum= (x,y)=>{
+    return x+y;
+}
+
+console.log(sum(5,7));
+
+const uzunluguBul = (str) =>{
+return str.length;
+};
+
+console.log(uzunluguBul("merhabaaaa"));
+
+// nfn kisa yolu
---
+++ day10/js/45-script.js
@@ -0,0 +1,3 @@
+const hesapla = (price, tax, discount) => {
+  
+}
