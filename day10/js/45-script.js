ec410a0870e6f4cecf3ed0f1d2eccf19c4ca47f1
Songül Çelik <songulcelik3537@gmail.com>
2023-11-24 22:33:08 +0300

    d2

--- day10/js/45-script.js
+++ day10/js/45-script.js
@@ -1,3 +1,30 @@
 const hesapla = (price, tax, discount) => {
-  
+  price ??=0
+  tax ??=0
+  discount ??=0
+
+  if(discount<=0) tax=0;
+  if(price>=1500) discount+=10;
+
+  let totalTax= (price*tax)/100;
+  let totalDiscount=(price*discount)/100;
+
+  let sonuc= price+totalTax - totalDiscount;
+  return sonuc;
 }
+
+console.log(hesapla(1000,20,50));
+
+const sonucuGoster = () => {
+  let priceEl=document.querySelector("#inpFiyat");
+  let price=Number(priceEl.value);
+  let taxEl= document.querySelector("#inpKdv");
+  let tax=Number(taxEl.value);
+  let discountEl=document.querySelector("#inpOran");
+  let discount=Number(discountEl.value);
+
+ const totalResult= hesapla(price,tax,discount);
+ document.querySelector("#snc").innerHTML=` sonuc: ${totalResult.toFixed(2)} USD`; 
+}
+
+sonucuGoster();
