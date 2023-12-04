8c8a5049619f916c1dbc739e354ad733e8ef47ea
Songül Çelik <songulcelik3537@gmail.com>
2023-12-04 23:00:24 +0300

    d-2

--- day17/js/82-script.js
+++ day17/js/82-script.js
@@ -1,36 +1,41 @@
 import { students } from "../data/students.js";
-
 document.getElementById("btnPoint").addEventListener("click",()=>{
-   let pointEl= document.querySelectorAll("#tblStudents tbody tr td:last-child");
-   //console.log(pointEl);
-
-   
-   
-   pointEl.forEach((point,index)=>{
-    let pointText=point.innerText;
-
-    
-
+  let pointEl=   document.querySelectorAll("#tblStudents tbody tr td:last-child")
+  //console.log(pointEl)
+  pointEl.forEach((point,index)=>{
+    let pointText=point.innerText
+    //console.log(pointText)
     if(pointText<50){
-        document.querySelector(`#tblStudents tbody tr:nth-child(${index+1})`).classList.add("table-danger")
+     /*  document.querySelector(`#tblStudents tbody tr:nth-child(${index+1})`).classList.add("table-danger") */
+      point.closest("tr").classList.add("table-danger")
+      
     }
-    
-   })
-   
-})
-
-
-const loadStudents = () => {
-  let strStudents="";
-  students.forEach((student,index)=>{
-    strStudents+=`<tr>
-                 <td>${index+1}</td>
-                 <td>${student.name}</td>
-                 <td>${student.point}</td>
-                    </tr>`
-                    document.querySelector("#tblStudents tbody").innerHTML=strStudents;
-
   })
-}
-
-loadStudents()
+})
+const loadStudents = () => { 
+    let strStudents="";
+    students.forEach((student,index)=>{
+strStudents+=`         <tr>
+                     <td>${index+1}</td>
+                     <td>${student.name}</td>  
+                    <td>${student.point}</td>  
+                    <td> <button class="btn btn-danger btn-delete">🗑️</button></td>  
+                    </tr>   `
+                    document.querySelector("#tblStudents tbody").innerHTML=strStudents
+    })
+ }
+ const deleteButtons = () => { 
+    document.querySelectorAll(".btn-delete").forEach((btnDel)=>{
+      btnDel.addEventListener("click",(e)=>{
+       
+    let name= e.target.closest("tr").children[1].innerText
+    const result=confirm(`${name} adli kisiyi silmek istediginize emin misiniz?`)
+    
+    //TODO!!!
+   /*  if(result){
+    } */
+      })
+    })
+  }
+ loadStudents()
+ deleteButtons()
---
+++ day18/data/students.js
@@ -0,0 +1,12 @@
+export const students = [
+  { id: 1, name: "Ayşe", point: 80 },
+  { id: 2, name: "Veli", point: 32 },
+  { id: 3, name: "Fatma", point: 56 },
+  { id: 4, name: "Ahmet", point: 90 },
+  { id: 5, name: "Zeynep", point: 98 },
+  { id: 6, name: "Ali", point: 75 },
+  { id: 7, name: "Can", point: 45 },
+  { id: 8, name: "Merve", point: 65 },
+  { id: 9, name: "Fatih", point: 87 },
+  { id: 10, name: "Hakan", point: 100 },
+];
